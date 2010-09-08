bb6f4302937e477f23c4de0d6a265d1d6f8985a0
odf <olaf.delgado@gmail.com>
2010-09-08 23:08:40 +1000

    Split off two functions from GeometryExporter.__init__

--- geometry_export.py
+++ geometry_export.py
@@ -7,6 +7,19 @@ reload(to_lux)
 import from_poser, to_lux
 
 
+def get_materials(geometry, convert = None):
+    f = convert or (lambda mat, k: ' NamedMaterial "%s/%s"' % (k, mat.Name()))
+    return [f(mat, geometry.material_key) for mat in geometry.materials]
+
+
+def preprocess(geometry, options = {}):
+    if options.get('compute_normals', True) in [True, 1, '1', 'true']:
+        geometry.compute_normals()
+    for i in xrange(int(options.get('subdivisionlevel', 0))):
+        print "  subdividing: pass", (i+1)
+        geometry.subdivide()
+
+
 class GeometryExporter(object):
     def __init__(self, subject, convert_material = None,
                  write_mesh_parameters = None, options = {}):
@@ -19,20 +32,8 @@ class GeometryExporter(object):
             print "Mesh has", geom.number_of_polygons, "polygons and",
             print geom.number_of_points, "vertices"
 
-            mats = geom.materials
-            key = geom.material_key
-            if convert_material:
-                materials = [convert_material(mat, key) for mat in mats]
-            else:
-                materials = [' NamedMaterial "%s/%s"' % (key, mat.Name())
-                             for mat in mats]
-
-            if options.get('compute_normals', True) in [True, 1, '1', 'true']:
-                geom.compute_normals()
-            for i in xrange(int(options.get('subdivisionlevel', 0))):
-                print "  subdividing: pass", (i+1)
-                geom.subdivide()
+            materials = get_materials(geom, convert_material)
+            preprocess(geom, options)
             to_lux.preprocess(geom)
-
             self.write = lambda file: to_lux.write(file, geom, materials,
                                                    write_mesh_parameters)
